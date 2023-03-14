6bb7ccb03db935316fdea3efcf855ecbe439776e
Gerard Darris <darrisgr@gmail.com>
2023-03-14 14:05:59 -0500

    Lecture ready

--- CarNS/Program.cs
+++ CarNS/Program.cs
@@ -1,3 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
+﻿using CarNS;
+Car car = new Car("Toyota", "Prius", 10, 50);
+Console.WriteLine(car.Make + " - " + car.Model);
